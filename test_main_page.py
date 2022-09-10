a79a372d779feee44f6b68b8c445243ff76c9ed0
Sasha <poksliun@icloud.com>
2022-09-10 22:34:43 +0200

    add_main_file

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
-from b
+from .pages.main_page import MainPage
 
-LINK = "http://selenium1py.pythonanywhere.com/"
-
+def test_guest_can_go_to_login_page():
+   LINK = "http://selenium1py.pythonanywhere.com/"
+   page = MainPage(browser, LINK)
+   page.open()
+   page.go_to_login_page()
