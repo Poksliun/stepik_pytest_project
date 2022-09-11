338cbb4f62daaa69821fbe3208647aac5f3ad725
Sasha <poksliun@icloud.com>
2022-09-11 15:02:31 +0200

    Добавлен login_page.py с проверками страницы логина добавлен pytest.ini с маркером на login_page В locator.py добавленны селекторы на форму логина, регистрации, проверки url Исправлен main_page на проверку с помощью assert в should_be_login_link В test_main_page.py добавлен тест проверки страницы логина

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,6 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
+import pytest
 
 def test_guest_can_go_to_login_page(browser):
    LINK = "http://selenium1py.pythonanywhere.com/"
@@ -8,7 +10,14 @@ def test_guest_can_go_to_login_page(browser):
 
 
 def test_guest_should_see_login_link(browser):
-   LINK = "http://selenium1py.pythonanywhere.com/"
+   LINK = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
    page = MainPage(browser, LINK)
    page.open()
    page.should_be_login_link()
+
+@pytest.mark.login_page
+def test_find_reg_log_form_and_logurl(browser):
+   LINK = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
+   login_page = LoginPage(browser, LINK)
+   login_page.open()
+   login_page.should_be_login_page()
