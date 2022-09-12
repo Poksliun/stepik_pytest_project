8d40a8600697082300071ffde1d08cdbc71b0046
Poksliun <poksliun@icloud.com>
2022-09-12 21:45:46 +0300

    Добавление функции проверки правильности добавленного товара

--- pages/product_page.py
+++ pages/product_page.py
@@ -6,3 +6,13 @@ class ProductPage(BasePage):
     def add_product_in_cart(self):
         button = self.browser.find_element(*ProductPageLocators.ADD_BUTTON)
         button.click()
+
+    def do_the_element_match(self):
+        self.does_the_name_math()
+
+    def does_the_name_match(self):
+        product_name = self.browser.find_element(*ProductPageLocators.NAME_PRODUCT).text
+        conf_product_name = self.browser.find_element(*ProductPageLocators.CONFIRMATION_NAME_PRODUCT)
+        assert product_name.lower() == conf_product_name.lower()
+    #
+    # def does_the_price_match(self):
