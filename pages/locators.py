c68b92288204b4e8e9daf3b9fe8fd4da92933547
Sasha <poksliun@icloud.com>
2022-09-12 22:08:04 +0200

    Добавлен локатор поиска цены товара на странице и поиска цены в подтверждении добавления товара в корзину

--- pages/locators.py
+++ pages/locators.py
@@ -11,3 +11,5 @@ class ProductPageLocators():
     ADD_BUTTON = (By.CSS_SELECTOR, '#add_to_basket_form > [value="Добавить в корзину"]')
     NAME_PRODUCT = (By.CSS_SELECTOR, '.col-sm-6.product_main>h1')
     CONFIRMATION_NAME_PRODUCT = (By.CSS_SELECTOR, "#messages :nth-child(1) strong")
+    PRICE_PRODUCT = (By.CSS_SELECTOR, '.col-sm-6.product_main .price_color')
+    CONFIRMATION_PRICE_PRODUCT = (By.CSS_SELECTOR, "#messages :nth-child(2) strong")
