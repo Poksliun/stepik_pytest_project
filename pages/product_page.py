524587088e2e3fb08a50cf0c17dc0b371ed1c844
Sasha <poksliun@icloud.com>
2022-09-12 22:08:53 +0200

    Добавлена функция сравнения цен товара на странице и добавленного в корзину

--- pages/product_page.py
+++ pages/product_page.py
@@ -12,7 +12,12 @@ class ProductPage(BasePage):
 
     def does_the_name_match(self):
         product_name = self.browser.find_element(*ProductPageLocators.NAME_PRODUCT).text
-        conf_product_name = self.browser.find_element(*ProductPageLocators.CONFIRMATION_NAME_PRODUCT).text
-        assert product_name.lower() == conf_product_name.lower()
-    #
-    # def does_the_price_match(self):
+        conf_product_name = self.browser.find_element\
+            (*ProductPageLocators.CONFIRMATION_NAME_PRODUCT).text
+        assert product_name.lower() == conf_product_name.lower(), 'Элементы не равны или не найдены'
+
+    def does_the_price_match(self):
+        product_price = self.browser.find_element(*ProductPageLocators.NAME_PRODUCT).text
+        conf_product_price = self.browser.find_element\
+            (*ProductPageLocators.CONFIRMATION_NAME_PRODUCT).text
+        assert product_price.lower() == conf_product_price.lower(), 'Элементы не равны либо не найдены'
