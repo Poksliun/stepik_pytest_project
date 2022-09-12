0d4269354721f624403f0d72a4952d1d7dbaa2fe
Sasha <poksliun@icloud.com>
2022-09-12 22:09:04 +0200

    Добавлена проверка сравнения цен товара на странице и добавленного в корзину

--- test_product_page.py
+++ test_product_page.py
@@ -2,9 +2,11 @@ from .pages.product_page import ProductPage
 import time
 
 def test_guest_can_add_product_to_basket(browser):
-    LINK = 'http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+    # LINK = 'http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+    LINK = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019'
     product_page = ProductPage(browser, LINK)
     product_page.open()
     product_page.add_product_in_cart()
     product_page.solve_quiz_and_get_code()
     product_page.does_the_name_match()
+    product_page.does_the_price_match()
