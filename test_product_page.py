c44631231ce227879cea0ec7360616a0b88bed2e
Poksliun <poksliun@icloud.com>
2022-09-12 21:46:21 +0300

    Начало написание тестав проверки лобавления товара в корзину

---
+++ test_product_page.py
@@ -0,0 +1,13 @@
+from .pages.product_page import ProductPage
+import time
+
+def test_guest_can_add_product_to_basket(browser):
+    LINK = 'http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+    product_page = ProductPage(browser, LINK)
+    product_page.open()
+    product_page.add_product_in_cart()
+    product_page.solve_quiz_and_get_code()
+    while True:
+        input_continue = input('Введите go для продолжения')
+        if input_continue == 'go':
+            False
