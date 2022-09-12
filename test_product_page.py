7856eff46d113339eeee3e8cf9f48e8cea0e3fa3
Sasha <poksliun@icloud.com>
2022-09-12 23:05:56 +0200

    Добавление параметризации проверки различных промокодов

--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,20 @@
+import pytest
 from .pages.product_page import ProductPage
 import time
 
-def test_guest_can_add_product_to_basket(browser):
-    # LINK = 'http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear'
-    LINK = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019'
+@pytest.mark.parametrize(
+    'promo',
+    [
+        pytest.param(x, marks=pytest.mark.xfail) if x == 7 else x for x in range(5,8)
+    ]
+)
+def test_guest_can_add_product_to_basket(browser,promo):
+    # LINK = 'http://selenium1py.pythonanywhere.com' \
+    #        '/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+    # LINK = 'http://selenium1py.pythonanywhere.com' \
+    #        '/ru/catalogue/coders-at-work_207/?promo=newYear2019'
+    LINK = f'http://selenium1py.pythonanywhere.com' \
+           f'/catalogue/coders-at-work_207/?promo=offer{promo}'
     product_page = ProductPage(browser, LINK)
     product_page.open()
     product_page.add_product_in_cart()
