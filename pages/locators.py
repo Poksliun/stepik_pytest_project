78036296be111676731d09c625bc9506f88c31ef
Sasha <poksliun@icloud.com>
2022-09-14 00:15:37 +0200

    Добавлены локаторы ссылка на корзину и товары в корзине

--- pages/locators.py
+++ pages/locators.py
@@ -14,3 +14,13 @@ class ProductPageLocators():
     PRICE_PRODUCT = (By.CSS_SELECTOR, '.col-sm-6.product_main .price_color')
     CONFIRMATION_PRICE_PRODUCT = (By.CSS_SELECTOR, "#messages :nth-child(2) strong")
     SUCCESS_MESSAGE = (By.CLASS_NAME, 'alert-success')
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_LINK = (By.CSS_SELECTOR, '.btn-group')
+
+class BasketPageLocators():
+    BASKET_PRODUCTS = (By.CSS_SELECTOR, '.basket-title.hidden-xs')
+
+
