5b1895eb62aa6cb979a8c6a54e5c92c02a82ab13
Poksliun <poksliun@icloud.com>
2022-09-14 15:34:34 +0300

    Добавлена функция проверки сравнения текста об отсутствии товара в корзине

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -6,3 +6,8 @@ class BasketPage(BasePage):
     def should_not_be_product_in_the_basket(self):
         assert self.is_not_element_present(*BasketPageLocators.BASKET_PRODUCTS), \
             "Success message is presented, but should not be"
+
+    def checking_the_text_about_an_empty_basket(self):
+       assert self.text_comparision(*BasketPageLocators.BASKET_PRODUCTS_TEXT,
+                                    'Your basket is empty. Continue shopping'), \
+           'Сообщение "Your basket is empty. Continue shopping" не совпадает с сообщением на сайте'
