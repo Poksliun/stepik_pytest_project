5503681522d30791470eba9acc928478657aa524
Poksliun <poksliun@icloud.com>
2022-09-15 10:32:57 +0300

    Добавлены коментарии.

--- pages/main_page.py
+++ pages/main_page.py
@@ -2,6 +2,10 @@ from .base_page import BasePage
 from .locators import MainPageLocators
 
 class MainPage(BasePage):
+    """
+    Класс главной страницы
+    """
     
     def __init__(self, *args, **kwargs):
+        #Наследует все функции из BasePage
         super(MainPage, self).__init__(*args, **kwargs)
