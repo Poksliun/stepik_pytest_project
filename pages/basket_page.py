9c3bb808123f70f52c4a7290da6e35d1c4285b74
Poksliun <poksliun@icloud.com>
2022-09-15 10:32:17 +0300

    Добавлены коментарии, переведены ответы assert на русский язык.

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -2,12 +2,17 @@ from .base_page import BasePage
 from .locators import BasketPageLocators
 
 class BasketPage(BasePage):
+    """
+    Класс страницы корзина
+    """
 
     def should_not_be_product_in_the_basket(self):
+        #Проверка отсутствия товара в корзине
         assert self.is_not_element_present(*BasketPageLocators.BASKET_PRODUCTS), \
-            "Success message is presented, but should not be"
+            "Отображается сообщение о товаре в корзине, но его не должно быть"
 
     def checking_the_text_about_an_empty_basket(self):
+        #Проверка текста об отсутствии товара в корзине
        assert self.text_comparision(*BasketPageLocators.BASKET_PRODUCTS_TEXT,
                                     'Your basket is empty. Continue shopping'), \
            'Сообщение "Your basket is empty. Continue shopping" не совпадает с сообщением на сайте'
