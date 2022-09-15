18b058df50472e16b7b71340a314826126b52599
Poksliun <poksliun@icloud.com>
2022-09-15 10:32:33 +0300

    Добавлены коментарии.

--- conftest.py
+++ conftest.py
@@ -3,17 +3,22 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
+    #Выбор языка и браузера
     parser.addoption('--browser_name',
                     action='store',
                     default="chrome",
                     help="Choose browser: chrome or firefox")
     parser.addoption('--language',
                     action='store',
-                    default='en',
+                    default=None,
                     help='Choose launguage: "en" or "ru"')
 
 @pytest.fixture(scope="function")
 def browser(request):
+    """
+    Фикстура запускающая браузер перед каждой вызванной функцией и
+    выключающая браузер после выполнения функции
+    """
     browser_name = request.config.getoption("browser_name")
     user_language = request.config.getoption("language")
     browser = None
