fb6023cf93026beb630482ecec690f67a7b190ea
Sasha <poksliun@icloud.com>
2022-09-18 10:39:56 +0200

    .

---
+++ trash.py
@@ -0,0 +1,8 @@
+# from selenium import webdriver
+#
+# brser = webdriver.Chrome()
+# brser.get('http://selenium1py.pythonanywhere.com/ru/accounts/login/')
+#
+# link = brser.current_url
+#
+# print(link)
